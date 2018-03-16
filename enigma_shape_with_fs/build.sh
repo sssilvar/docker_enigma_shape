346f7f5d06a6ae11038154e3f114a7e66d3864dc
Santiago Smith <sssilvar@unal.edu.co>
2018-03-16 10:08:44 -0500

    Final version (Freesurfer include inside the container)

--- enigma_shape_with_fs/build.sh
+++ enigma_shape_with_fs/build.sh
@@ -1,3 +1,6 @@
-docker build -t sssilvar/eshape_fs \
+#!/bin/bash
+
+# This script builds the docker image with tag sssilva/eshape:1.0
+docker build -t sssilvar/eshape_fs:1.0 \
 	--build-arg proxy=[http://user:pass@proxy_server.com:port/]\
 	$(pwd)
---
+++ run_enigma_shape_fs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Run the ENIGMA Shape analysis over a FreeSurfer processed dataset (with recon-all)
+# map the /group folder to the foder where the groupfile.csv is located in the local
+# map the /input foldet to the dataset processed
+# map the /output folder where you wnat the results to be saved
+# 
+# Example:
+# 
+# docker run -it \
+# 	-v <groupfile_csv_folder>:/group \
+# 	-v <dataset_folder>:/input \
+# 	-v <output_folder>:/output \
+# 	sssilvar/eshape_fs /group/groupfile.csv /input /output
+
+docker run -it \
+	-v /home/sssilvar/Documents/:/group \
+	-v /home/sssilvar/Documets/dataset/:/input \
+	-v /home/sssilvar/Documets/output:/output \
+	sssilvar/eshape_fs /group/groupfile.csv /input /output
