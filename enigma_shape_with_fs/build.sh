403625b7337d74e9e22d21df3fa7453a9423d8cb
Santiago Smith <sssilvar@unal.edu.co>
2018-03-16 10:12:30 -0500

    Final version (Freesurfer include inside the container)

--- enigma_shape_with_fs/build.sh
+++ enigma_shape_with_fs/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # This script builds the docker image with tag sssilva/eshape:1.0
+# To set the proxy use this format: [http://user:pass@proxy_server.com:port/]
 docker build -t sssilvar/eshape_fs:1.0 \
-	--build-arg proxy=[http://user:pass@proxy_server.com:port/]\
+	--build-arg proxy=$1\
 	$(pwd)
